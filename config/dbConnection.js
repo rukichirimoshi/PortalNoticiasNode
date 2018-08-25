ddac98edc779591a435db6f7b8f3bb91f1a02559
rukichirimoshi <vvvolkmann@gmail.com>
2018-08-25 15:23:39 -0300

    Consign agora carrega o módulo de conexão

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
 
-module.exports = function(){
-    return connection = mysql.createConnection({
+var connection = function() {
+    return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '',
         database: 'portal_noticias'
     });
+}
+
+module.exports = function() {
+    return connection;
 }
